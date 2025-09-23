eb0feaf139c3b74fd0b47646892539593ea55a72
unknown <ovo_gh3@hotmail.com>
2025-09-23 14:51:59 -0700

    added auth guards to pharmacist endpoints

--- src/pharmacist-profiles/pharmacist-profiles.controller.ts
+++ src/pharmacist-profiles/pharmacist-profiles.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { PharmacistProfilesService } from './pharmacist-profiles.service';
 import { CreatePharmacistProfileDto } from './dto/create-pharmacist-profile.dto';
 import { UpdatePharmacistProfileDto } from './dto/update-pharmacist-profile.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { PharmacistProfileEntity } from './entities/pharmacist-profile.entity';
+import { FirebaseAuthGuard } from 'src/auth/firebase-auth.guard';
 
 @Controller('pharmacist-profiles')
 @ApiTags('Pharmacist Profiles')
@@ -11,30 +12,40 @@ export class PharmacistProfilesController {
   constructor(private readonly pharmacistProfilesService: PharmacistProfilesService) {}
 
   @Post()
+  @UseGuards(FirebaseAuthGuard)
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: PharmacistProfileEntity })
   create(@Body() createPharmacistProfileDto: CreatePharmacistProfileDto) {
     return this.pharmacistProfilesService.create(createPharmacistProfileDto);
   }
 
   @Get()
+  @UseGuards(FirebaseAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PharmacistProfileEntity, isArray: true })
   findAll() {
     return this.pharmacistProfilesService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(FirebaseAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PharmacistProfileEntity })
   findOne(@Param('id') id: string) {
     return this.pharmacistProfilesService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(FirebaseAuthGuard)
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: PharmacistProfileEntity })
   update(@Param('id') id: string, @Body() updatePharmacistProfileDto: UpdatePharmacistProfileDto) {
     return this.pharmacistProfilesService.update(id, updatePharmacistProfileDto);
   }
 
   @Delete(':id')
+  @UseGuards(FirebaseAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: PharmacistProfileEntity })
   remove(@Param('id') id: string) {
     return this.pharmacistProfilesService.remove(id);
--- src/pharmacist-profiles/pharmacist-profiles.module.ts
+++ src/pharmacist-profiles/pharmacist-profiles.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PharmacistProfilesService } from './pharmacist-profiles.service';
 import { PharmacistProfilesController } from './pharmacist-profiles.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [PharmacistProfilesController],
-  providers: [PharmacistProfilesService],
-  imports: [PrismaModule],
+  providers: [PharmacistProfilesService, UsersService],
+  imports: [PrismaModule, AuthModule],
 })
 export class PharmacistProfilesModule {}
